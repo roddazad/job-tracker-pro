9bd8df26c9535f4da8ae92751158456c63a3bf9b
roddazad <roddazad@gmail.com>
2025-05-29 19:25:18 -0700

    Add modularized Header with user greeting logic and finalize sign-in/sign-up flow UI

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -4,6 +4,7 @@ import Sidebar from '../../components/Sidebar';
 import Header from '../../components/Header';
 
 export default function DashboardLayout({ children }) {
+  
   return (
     <div className="min-h-screen bg-gray-100 text-gray-900">
     {/* Sidebar */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 export default function Header() {
+   const user = {
+    name: 'Rodd',
+    isAuthenticated: true,
+  };
+
   return (
     <header className="bg-white shadow-sm p-4 flex justify-between items-center z-10">
-      <h1 className="text-lg font-semibold">Welcome back, Rodd 👋</h1>
-      <button className="text-sm bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
-        Logout
-      </button>
-    </header>
+          <h1 className="text-lg font-semibold">
+            {user.isAuthenticated
+              ? `Welcome back, ${user.name} 👋`
+              : 'Welcome back 👋'}
+          </h1>
+          <button className="text-sm bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition">
+            Logout
+          </button>
+        </header>
   );
 }
